e4ba27672372bf4f2a4d7378207cb58c8650fc98
amberorrange <orrangeamber@gmail.com>
2021-05-12 22:27:45 +0000

    may have completed route /add_to_cart/<melom_id>

--- shoppingsite.py
+++ shoppingsite.py
@@ -6,7 +6,7 @@ put melons in a shopping cart.
 Authors: Joel Burton, Christian Fernandez, Meggie Mahnken, Katie Byers.
 """
 
-from flask import Flask, render_template, redirect, flash
+from flask import Flask, render_template, redirect, flash, session
 import jinja2
 
 import melons
@@ -50,7 +50,7 @@ def show_melon(melon_id):
     Show all info about a melon. Also, provide a button to buy that melon.
     """
 
-    melon = melons.get_by_id("meli")
+    melon = melons.get_by_id(melon_id)
     print(melon)
     return render_template("melon_details.html",
                            display_melon=melon)
@@ -89,6 +89,13 @@ def add_to_cart(melon_id):
     page and display a confirmation message: 'Melon successfully added to
     cart'."""
 
+    session['cart'] = session.get('cart', {})
+
+    session['cart'][melon_id] = session['cart'].get(melon_id, 0) + 1
+
+
+    flash("You've successfully added to your cart!")
+
     # TODO: Finish shopping cart functionality
 
     # The logic here should be something like:
@@ -100,7 +107,7 @@ def add_to_cart(melon_id):
     # - flash a success message
     # - redirect the user to the cart page
 
-    return "Oops! This needs to be implemented!"
+    return redirect("/cart")
 
 
 @app.route("/login", methods=["GET"])
